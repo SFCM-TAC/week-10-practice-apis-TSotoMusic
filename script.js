9f0b4ca81c18653a335333445c1062fe31066120
Thomas <Thomas@Thomass-MacBook-Pro-2.local>
2018-11-14 16:14:54 -0800

    Adding drop down button

--- script.js
+++ script.js
@@ -30,7 +30,33 @@ species.send();
 species.onload = function() {
   var response = species.response;
   var parsed = JSON.parse(response);
-  console.log(parsed);
-
+  parsed.results.name = speciesA
+  console.log(speciesA);
+// for (i=0; i < speciesA.length; i++) {
+// }
 }
+}
+
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function speciesList() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
